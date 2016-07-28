60c6a3a27c3029c88378adaebe1d1022438fc48e
Arrrlex <arrrlex.mck@gmail.com>
2016-07-28 12:02:43 +0100

    add templates for project pages

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^pi/$', views.pi, name='project-pi'),
+	url(r'^e/$', views.e, name='project-e'),
+	url(r'^fibonacci/$', views.fibonacci, name='project-fibonacci'),
+	url(r'^prime-factors/$', views.prime_factors, name='project-prime-factors'),
+	url(r'^binary-converter/$', views.binary_converter, name='project-binary-converter'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -8,3 +8,18 @@ projlist = [dir for dir in listdir() if isdir(dir)]
 
 def index(request):
 	return render(request, 'projects/index.html', {'current_page': 'portfolio'})
+
+def pi(request):
+	return render(request, 'projects/pi.html', {'current_page': 'portfolio'})
+
+def e(request):
+	return render(request, 'projects/e.html', {'current_page': 'portfolio'})
+
+def fibonacci(request):
+	return HttpResponse('Dummy View.')
+
+def prime_factors(request):
+	return HttpResponse('Dummy View.')
+
+def binary_converter(request):
+	return HttpResponse('Dummy View.')
