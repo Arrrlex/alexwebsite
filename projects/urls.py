639d60ebb9f1b2c4219d9529dea522828c3409a6
Arrrlex <arrrlex.mck@gmail.com>
2016-08-10 17:24:12 +0100

    refactored karan projects into one page

--- projects/urls.py
+++ projects/urls.py
@@ -12,29 +12,9 @@ urlpatterns = [
 	url(r'^$',
 		TemplateView.as_view(template_name='projects/index.html'),
 		name='index'),
-	url(r'^pi/$',
-		views.project,
-		kwargs={
-		'template_page': 'projects/pi.html',
-		'submit': 'projects:pi',
-		'calculate': calculate_pi
-		},
-		name='pi'),
-	url(r'^e/$',
-		views.project,
-		kwargs={
-		'template_page': 'projects/e.html',
-		'submit': 'projects:e',
-		'calculate': calculate_e
-		},
-		name='e'),
-	url(r'^fibonacci/$',
-		views.project,
-		kwargs={
-		'template_page': 'projects/fibonacci.html',
-		'submit': 'projects:fibonacci',
-		'calculate': fib},
-		name='fibonacci'),
+	url(r'^karan/$',
+		views.karan_project,
+		name='karan'),
 	url(r'^prime-factors/$', views.prime_factors, name='prime-factors'),
 	url(r'^binary-converter/$', views.binary_converter, name='binary-converter'),
 ]
--- projects/views.py
+++ projects/views.py
@@ -15,20 +15,31 @@ def prime_factors(request):
 def binary_converter(request):
 	return HttpResponse('Dummy View.')
 
-def project(request, current_page, template_page, submit, calculate):
-	context = {'current_page': current_page, 'submit': submit}
+def karan_project(request, current_page):
+	context = {'current_page': current_page, 'func':'calculate_pi', 'arg': '50'}
 	try:
-		input_string = request.GET['arg']
+		input_arg = request.GET['arg']
+		context['arg'] = input_arg
 	except:
-		return render(request, template_page, context)
+		return render(request, 'projects/applet-karan.html', context)
 	try:
-		arg = int(input_string)
+		input_func = request.GET['fun']
+		context['func'] = input_func
+	except:
+		return render(request, 'projects/applet-karan.html', context)
+	try:
+		arg = int(input_arg)
 	except:
 		context['error_message'] = 'Please enter a valid number'
-		return render(request, template_page, context)
+		return render(request, 'projects/applet-karan.html', context)
 	if not 1 <= arg <= 1000:
 		context['error_message'] = 'Please enter a number between 1 and 1000, to avoid large strain on the server.'
-		return render(request, template_page, context)
-	result = calculate(arg)
+		return render(request, 'projects/applet-karan.html', context)
+	try:
+		fun = eval(input_func)
+	except:
+		context['error_message'] = 'Please choose a valid script'
+		return render(request, 'projects/applet-karan.html', context)
+	result = fun(arg)
 	context.update({'arg': arg, 'result': result})
-	return render(request, template_page, context)
+	return render(request, 'projects/applet-karan.html', context)
