bccd458b2c4eb0ece38de249cab1593238aea85d
Arrrlex <arrrlex.mck@gmail.com>
2016-08-12 00:05:45 +0100

    fixed tests following template modification

--- projects/tests.py
+++ projects/tests.py
@@ -8,27 +8,27 @@ from django.core.urlresolvers import reverse
 class PiViewTests(TestCase):
 
 	def test_pi_50(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'fun':'calculate_pi'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'func':'pi'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "3.1415926535897932384626433832795028841971693993751")
 
-	def test_pi_empty_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'', 'fun':'calculate_pi'})
+	def test_pi_emptying(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'', 'func':'pi'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
-	def test_pi_invalid_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','fun':'calculate_pi'})
+	def test_pi_invaliding(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','func':'pi'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
 	def test_pi_too_big(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'calculate_pi'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'pi'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
 
 	def test_pi_too_small(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'calculate_pi'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'pi'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
 
@@ -49,53 +49,80 @@ class KaranScriptsTests(TestCase):
 class EViewTests(TestCase):
 
 	def test_e_50(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'fun':'calculate_e'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'func':'e'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "2.7182818284590452353602874713526624977572470936999")
 
-	def test_e_empty_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'', 'fun':'calculate_e'})
+	def test_e_emptying(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'', 'func':'e'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
-	def test_e_invalid_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','fun':'calculate_e'})
+	def test_e_invaliding(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','func':'e'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
 	def test_e_too_big(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'calculate_e'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'e'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
 
 	def test_e_too_small(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'calculate_e'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'e'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
 
 class FibViewTests(TestCase):
 
 	def test_fib_50(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'fun':'fib_str'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'func':'fib'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "12586269025")
 
-	def test_fib_empty_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'', 'fun':'fib_str'})
+	def test_fib_emptying(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'', 'func':'fib'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
-	def test_fib_invalid_string(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','fun':'fib_str'})
+	def test_fib_invaliding(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','func':'fib'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a valid number")
 
 	def test_fib_too_big(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'fib_str'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'fib'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
 
 	def test_fib_too_small(self):
-		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'fun':'fib_str'})
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'fib'})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Please enter a number between 1 and 1000")
+
+class PrimeFactorViewTests(TestCase):
+
+	def test_prime_factors_50(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'50', 'func':'prime_factors'})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "2 &times; 5<sup>2</sup>")
+
+	def test_e_emptying(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'', 'func':'prime_factors'})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Please enter a valid number")
+
+	def test_e_invaliding(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'I am not a number','func':'prime_factors'})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Please enter a valid number")
+
+	def test_e_too_big(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'prime_factors'})
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, "Please enter a number between 1 and 1000")
+
+	def test_e_too_small(self):
+		response = self.client.get(reverse('projects:karan'), {'arg':'4000', 'func':'prime_factors'})
 		self.assertEqual(response.status_code, 200)
 		self.assertContains(response, "Please enter a number between 1 and 1000")
