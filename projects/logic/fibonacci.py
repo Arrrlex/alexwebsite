17d3327edea6138059b751c2f55bedf0c1920d9c
Arrrlex <arrrlex.mck@gmail.com>
2016-08-12 22:30:23 +0100

    made fibonacci function more efficient

--- projects/logic/fibonacci.py
+++ projects/logic/fibonacci.py
@@ -1,7 +1,27 @@
-fib_dict = {1:1, 2:1}
+# We use the following two identities:
+# fib(2*k) = fib(k)*(2*fib(k+1)-fib(k))
+# fib(2*k + 1) = fib(k+1)**2 + fib(k)**2
 
 def fib(n):
-    if n not in fib_dict:
-        fib_dict[n] = fib(n-1) + fib(n-2)
-
-    return fib_dict[n]
+	op_stack = []
+	curr, next = 1, 1
+	while n != 1:
+		if n % 2 == 0:
+			n = n / 2
+			op_stack.append("double")
+		else:
+			n -= 1
+			op_stack.append("increment")
+	while len(op_stack) != 0:
+		op = op_stack.pop()
+		if op == "double":
+			n *= 2
+			curr, next = (
+				curr * (2 * next - curr),
+				curr**2 + next**2)
+		if op == "increment":
+			n += 1
+			curr, next = (
+				next,
+				curr + next)
+	return curr
