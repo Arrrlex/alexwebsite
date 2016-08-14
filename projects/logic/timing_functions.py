412cd6f9a5d8f7d0c524bd39df2a6e0d1c93fd21
Arrrlex <arrrlex.mck@gmail.com>
2016-08-14 20:30:46 +0100

    Made function timing script more robust

--- projects/logic/timing_functions.py
+++ projects/logic/timing_functions.py
@@ -1,30 +1,39 @@
 from timeit import timeit
 import random
 
-def time_function(func, import_location, arg):
+def time_function(func, import_location, arg, repeat=3):
 	print('Arg: {}'.format(arg))
 	stmt = '{}({})'.format(func, arg)
 	stup = 'from {} import {}'.format(import_location, func)
-	timetaken = timeit(stmt, stup, number=1)
+	timetaken = timeit(stmt, stup, number=repeat)/repeat
 	print('Time: {}'.format(timetaken))
 	return timetaken
 
-def find_timeout_level(func, import_location='__main__', timeout=2):
-	timetaken = 0
-	arg = 1
-	while timetaken < timeout:
-		arg = random.randint(arg*2, arg*3)
-		timetaken = time_function(func, import_location, arg)
-	upper_bound = arg
-	lower_bound = arg // 2
-	for i in range(10):
-		new = (upper_bound + lower_bound)//2
-		if new == upper_bound or new == lower_bound:
-			break
-		timetaken = time_function(func, import_location, new)
-		if timetaken < timeout:
-			lower_bound = new
-		else:
-			upper_bound = new
+def find_timeout_level(func, import_location='__main__', timeout=2, trials=5):
+	results = []
+	for i in range(trials):
+		timetaken = 0
+		arg = 1
+		while timetaken < timeout:
+			arg = random.randint(arg*2, arg*3)
+			timetaken = time_function(func, import_location, arg)
+		upper_bound = arg
+		lower_bound = arg // 2
+		for i in range(10):
+			new = (upper_bound + lower_bound)//2
+			if new == upper_bound or new == lower_bound:
+				break
+			timetaken = time_function(func, import_location, new)
+			if timetaken < timeout:
+				lower_bound = new
+			else:
+				upper_bound = new
+		results.append(lower_bound)
+		print('------')
+	result = sum(results)/len(results)
+	return result
 
-	return lower_bound
+# Running find_timeout_level on server for various functions and timeout=2:
+
+# calculate_pi: 878.4
+# calculate_e: 
