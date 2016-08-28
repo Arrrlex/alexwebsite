1897ccc55c2cac551f814e4dd7884bc77620e251
Arrrlex <arrrlex.mck@gmail.com>
2016-08-28 10:08:42 +0100

    improved overlay positioning code

--- projects/static/projects/script.js
+++ projects/static/projects/script.js
@@ -7,27 +7,16 @@ $( document ).ready(function() {
 			+ " &times; " 
 			+ $(this).data("height") 
 			+ " m<sup>2</sup>");
-		// Calculating the x-offset
-		var mainML = getCSSVal('main', 'margin-left');
-		var bodyML = getCSSVal('body', 'margin-left');
-		var xOffset = mainML + bodyML;
-		// Calculating the y-offset
-		var bodyMT = getCSSVal('body', 'margin-top');
-		var titleH = getCSSVal('h1', 'height');
-		var titleMB = getCSSVal('h1', 'margin-bottom');
-		var menuH = getCSSVal('nav', 'height');
-		var mainMT = getCSSVal('main', 'margin-top');
-		var yOffset = bodyMT + titleH + titleMB + menuH + mainMT;
-		overlay.css("left", pos.left - xOffset);
-		overlay.css("top", pos.top + window.pageYOffset - yOffset);
+		var parentOffset = overlay.parent().offset();
+		var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+
+		overlay.css("left", pos.left - parentOffset.left);
+		overlay.css("top", pos.top + scrollTop - parentOffset.top);
+		// overlay.css("top", pos.top + window.pageYOffset - parentOffset.top);
+
 		overlay.css("width", Math.max(pos.width, 100));
 	}, function() {
 		var overlay = $( "#overlay" );
 		overlay.css("visibility", "hidden");
 	});
 });
-
-function getCSSVal(selector, attr) {
-	var valStr = $( selector ).css(attr);
-	return parseInt(valStr.substring(0, valStr.length - 2));
-}
