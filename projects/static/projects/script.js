af687b1444d1f48e53661992f03b85fc9e39e7ba
Arrrlex <arrrlex.mck@gmail.com>
2016-08-28 10:15:39 +0100

    removed commented-out line

--- projects/static/projects/script.js
+++ projects/static/projects/script.js
@@ -9,11 +9,8 @@ $( document ).ready(function() {
 			+ " m<sup>2</sup>");
 		var parentOffset = overlay.parent().offset();
 		var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-
 		overlay.css("left", pos.left - parentOffset.left);
 		overlay.css("top", pos.top + scrollTop - parentOffset.top);
-		// overlay.css("top", pos.top + window.pageYOffset - parentOffset.top);
-
 		overlay.css("width", Math.max(pos.width, 100));
 	}, function() {
 		var overlay = $( "#overlay" );
